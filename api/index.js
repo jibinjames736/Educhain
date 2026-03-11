29440eb9e50e94a3398e43832f7883529bf4d39c
jibinjames736 <jibinjames736@gmail.com>
2026-03-11 02:53:14 +0000

    updated import path in index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../backend/app');  
+const app = require('../backend/src/app');   // ✅ correct path if app.js is inside backend/src
 
 module.exports.handler = serverless(app);
