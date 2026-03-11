f70704005fda1557799587b2714bd860f45b1426
jibinjames736 <jibinjames736@gmail.com>
2026-03-11 03:44:56 +0000

    changed api folder location

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../backend/src/app');   // ✅ correct path if app.js is inside backend/src
-
-module.exports.handler = serverless(app);
